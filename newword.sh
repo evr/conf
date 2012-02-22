eddc94a9fb59ee690a8121a66651e67aeedc1120
omegaga <omegaleedh@gmail.com>
2012-02-22 19:38:56 +0800

    NEW function: if word exists then do NOT add it.

--- newword.sh
+++ newword.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 WORD_DIR=~/.word
+dict $@ -d wn;
 
 if [ ! -f "$WORD_DIR" ]
 then 
-    touch "$WORD_DIR"
+    touch $WORD_DIR
     echo "new file .word created!"
 fi
 
-read -p "Do you want to save the word? Y/n:" choice
-
-if [ "$choice" = "y" -o "$choice" = "Y" ]; then
-    echo $@ >> "$WORD_DIR";
-    echo "Word successfully saved!"
+if egrep -q -i "\<$@\>" $WORD_DIR 
+then
+    echo "Word exists!"
 else
-    echo "No word saved!"
+    read -p "Do you want to save the word? Y/n:" choice
+    if [ "$choice" = "n" -o "$choice" = "N" ]; then
+        echo "No word saved!"
+    else
+        echo $@ >> "$WORD_DIR" ||echo "Fail to save word!"
+        echo "Word successfully saved!"
+    fi
 fi
